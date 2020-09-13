fad26088a437f4fc16022379ec3fe8abf571bdac
ajss25 <61304376+ajss25@users.noreply.github.com>
2020-09-13 13:51:12 -0700

    revert changes

--- MindTracks/MindTracks/settings.py
+++ MindTracks/MindTracks/settings.py
@@ -133,15 +133,3 @@ CRISPY_TEMPLATE_PACK='bootstrap4'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
