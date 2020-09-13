4285204adeb2eb954e6857a5a187e740fdda119d
ajss25 <61304376+ajss25@users.noreply.github.com>
2020-09-13 13:46:51 -0700

    added whitenoise

--- MindTracks/MindTracks/settings.py
+++ MindTracks/MindTracks/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+from whitenoise import WhiteNoise
 from pathlib import Path
 import os
 
